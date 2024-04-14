30d867766f59f411f87fdff39f8664ed175d97b5
Reid Lynch <reid.lynch@gmail.com>
2024-04-14 10:05:19 -0400

    prepare for publish

--- mix.exs
+++ mix.exs
@@ -1,27 +1,46 @@
 defmodule Codeowners.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/reid-rigo/codeowners"
+  @version "0.1.0"
+
   def project do
     [
       app: :codeowners,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: @source_url
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    A pure Elixir CODEOWNERS parser.
+    """
+  end
+
+  defp package do
+    [
+      name: "Codeowners",
+      files: ~w(lib mix.exs README.md CHANGELOG.md LICENSE),
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
