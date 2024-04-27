bced0249372f037ae649c4fbb34bd5e3f295b121
Reid Lynch <reid.lynch@gmail.com>
2024-04-27 09:16:12 -0400

    0.1.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-# Unreleased
+# 0.1.6
 
 Allow specifying root when calling load or build as alternative to `File.cwd!`
 
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Codeowners.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/reid-rigo/codeowners"
-  @version "0.1.5"
+  @version "0.1.6"
 
   def project do
     [
